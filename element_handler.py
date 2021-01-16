e5303d05c9d9fdc26cbcd1c52ade86faf7a37433
Panelatta <dennis.kuo@outlook.com>
2021-01-16 19:13:28 +0800

    Fixed the display of interline block formula.

--- element_handler.py
+++ element_handler.py
@@ -1,5 +1,6 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 import codecs
+import re
 
 class TagNotPError(Exception):
     pass
@@ -14,40 +15,58 @@ def p_handler(p_tag, logger):
     if p_tag.name != 'p':
         raise TagNotPError('Received a tag which is not <p>.')
     
-    # logger.debug(len(p_tag.contents))
-    # logger.debug(p_tag.contents)
-    # content = p_tag.contents[0].strip().replace('$$$', '$')
+    # When a piece of text *a* is followed by a piece of bold/italic/
+    # typewriter-fonted text *b*, checking whether a[-1] is '"' is
+    # needed. If not so, *b* should be added a space at its beginning.
+    def space_add(contents, tmp_str, index):
+        if index > 0:
+            prev_item = contents[index - 1]
+            if isinstance(prev_item, element.Tag):
+                if prev_item.contents[0][-1] != '"':
+                    tmp_str = ' ' + tmp_str
+            else:
+                if prev_item[-1] != '"':
+                    tmp_str = ' ' + tmp_str
+
+        return tmp_str
+
+    # When a latex formula is presented as a interline block, the string
+    # '$$' at its both ends need to be processed as '$$\n' and '\n$$'.
+    # 
+    # That's because some Markdown parsing engines cannot process the
+    # '$$...$$' correctly, and just parse it as an inline formula, like
+    # Typora.
+    def interline_formula_process(tmp_str):
+        matcher = re.search(r'^\$\$([^\$]*)\$\$$', tmp_str)
+        if matcher is not None:
+            tmp_str = '$$\n' + matcher.group(1) + '\n$$'
+
+        return tmp_str
 
     content_list = []
     for index, item in enumerate(p_tag.contents):
         if hasattr(item, 'get') == False:
             tmp_str = item.strip().replace('$$$', '$')
-            if index > 0:
-                tmp_str = ' ' + tmp_str
+            tmp_str = interline_formula_process(tmp_str)
+            tmp_str = space_add(p_tag.contents, tmp_str, index)
             content_list.append(tmp_str)
         else:
             item_span_class = item.get('class')
             if item_span_class[0] == 'tex-font-style-tt':
                 tmp_str = '$\\texttt{' + item.contents[0].strip().replace('$$$', '$') + '}$'
-                if index > 0:
-                    tmp_str = ' ' + tmp_str
+                tmp_str = space_add(p_tag.contents, tmp_str, index)
                 content_list.append(tmp_str)
             elif item_span_class[0] == 'tex-font-style-bf':
                 tmp_str = '**' + item.contents[0].strip().replace('$$$', '$') + '**'
-                if index > 0:
-                    tmp_str = ' ' + tmp_str
+                tmp_str = space_add(p_tag.contents, tmp_str, index)
                 content_list.append(tmp_str)
             elif item_span_class[0] == 'tex_font_style-it':
                 tmp_str = '*' + item.contents[0].strip().replace('$$$', '$') + '*'
-                if index > 0:
-                    tmp_str = ' ' + tmp_str
+                tmp_str = space_add(p_tag.contents, tmp_str, index)
                 content_list.append(tmp_str)
-            else:
-                logger.debug('=================')
-                logger.debug(item_span_class)
-                logger.debug(item.contents[0])
     
     content = ''.join(content_list)
+
     return content
 
 def ul_handler(ul_tag, logger):
@@ -59,18 +78,6 @@ def ul_handler(ul_tag, logger):
         for item
         in ul_tag.find_all('li')
     ]
-
-    # ul_contents = []
-    # for item in ul_tag.find_all('li'):
-    #     for cont in item.contents:
-    #         logger.debug('=======================')
-    #         logger.debug(type(cont))
-    #         a = cont.strip()
-    #         logger.debug(type(a))
-    #         a = cont.replace('$$$', '$')
-    #         logger.debug(type(a))
-    #         # ul_contents.append('-' + cont.strip().replace('$$$', '$'))
-    #         ul_contents.append('-' + a)
 
     return ul_contents
 
